b631cd999e8052a9f63f025f2512422fc0c2b14c
Dominik Kopec <kopec.dominik@gmai.com>
2023-04-05 22:21:07 -0400

    fix: resource name

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_log_analytics_workspace" "this" {
   retention_in_days   = 30
 }
 
-resource "azurerm_container_app_environment" "example" {
+resource "azurerm_container_app_environment" "this" {
   name                       = "cae-${local.resource_postfix}"
   location                   = azurerm_resource_group.this.location
   resource_group_name        = azurerm_resource_group.this.name
